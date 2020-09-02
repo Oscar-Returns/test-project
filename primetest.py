2718e9f5d75392f39fbe9385af2af2adf879dd51
Oscar-Returns <70572014+Oscar-Returns@users.noreply.github.com>
2020-09-02 14:06:59 +0100

    Add files via upload
    cleaned up and now begins from 1 and doesn't have 2 hardcoded in.

--- primetest.py
+++ primetest.py
@@ -1,5 +1,5 @@
-currentnumber = 2#                          [won't be accurate if you try starting from a higher number.]
-prime1 = 2                            
+currentnumber = 1#                          [won't be accurate if you try starting from a higher number.]
+prime1 = 0
 prime2 = 0                      
 prime3 = 0
 prime4 = 0
@@ -8,63 +8,60 @@ prime6 = 0
 prime7 = 0
 prime8 = 0          #                       [you'll have to manually add more variables              ]
 prime9 = 0          #                       [to increase the accurate range.                         ]
-legacyvariable1 = 0#                       [variables up to prime n gives correct results up to n^2.]                     
-
+                    #                       [variables up to prime n gives correct results up to n^2.]                     
 while currentnumber < 841:                 #[remember to increase to prime n^2 if your expanding the range.]
    
-    mod = currentnumber % prime1 
-    if mod == 0 and currentnumber != prime1:
+    if prime2 !=0 and currentnumber % prime1 == 0:
         print(currentnumber)
         print("is not prime")
         currentnumber = currentnumber + 1
    
-    elif prime2 !=0 and currentnumber % prime2 == 0 and currentnumber != prime2:
+    elif prime2 !=0 and currentnumber % prime2 == 0:
         print(currentnumber)
         print("is not prime")
         currentnumber = currentnumber + 1
     
-    elif prime3 !=0 and currentnumber % prime3 == 0 and currentnumber != prime3:
+    elif prime3 !=0 and currentnumber % prime3 == 0:
         print(currentnumber)
         print("is not prime")
         currentnumber = currentnumber + 1
    
-    elif prime4 !=0 and currentnumber % prime4 == 0 and currentnumber != prime4:
+    elif prime4 !=0 and currentnumber % prime4 == 0:
         print(currentnumber)
         print("is not prime")
         currentnumber = currentnumber + 1
    
-    elif prime5 !=0 and currentnumber % prime5 == 0 and currentnumber != prime5:
+    elif prime5 !=0 and currentnumber % prime5 == 0:
         print(currentnumber)
         print("is not prime")
         currentnumber = currentnumber + 1
    
-    elif prime6 !=0 and currentnumber % prime6 == 0 and currentnumber != prime6:
+    elif prime6 !=0 and currentnumber % prime6 == 0:
         print(currentnumber)
         print("is not prime")
         currentnumber = currentnumber + 1
   
-    elif prime7 !=0 and currentnumber % prime7 == 0 and currentnumber != prime7:
+    elif prime7 !=0 and currentnumber % prime7 == 0:
         print(currentnumber)
         print("is not prime")
         currentnumber = currentnumber + 1
    
-    elif prime8 !=0 and currentnumber % prime8 == 0 and currentnumber != prime8:
+    elif prime8 !=0 and currentnumber % prime8 == 0:
         print(currentnumber)
         print("is not prime")
         currentnumber = currentnumber + 1
     
-    elif prime9 !=0 and currentnumber % prime9 == 0 and currentnumber != prime9:# [you'll have to add more of these         ]
+    elif prime9 !=0 and currentnumber % prime9 == 0:                            # [you'll have to add more of these         ]
         print(currentnumber)                                                    # [to increase the range. remember to change]
         print("is not prime")                                                   # [ALL the relevant variables on each one.  ]
         currentnumber = currentnumber + 1
     
-    else:
+    elif currentnumber != 1:
         print(currentnumber)
         print("is prime")
-        #if legacyvariable1 == 0:                                                 [you'll have do to uncomment this]
-        #    prime1 = currentnumber                                               [and do other stuff to start     ]
-        #    legacyvariable1 = 1                                                  [from 1 and avoid hardcoding in 2.]
-        if prime2 == 0 and currentnumber != 2:
+        if prime1 == 0:                                         
+            prime1 = currentnumber                                              
+        elif prime2 == 0:
             prime2 = currentnumber
         elif prime3 == 0:
             prime3 = currentnumber
@@ -81,3 +78,6 @@ while currentnumber < 841:                 #[remember to increase to prime n^2 i
         elif prime9 == 0:                #                                       [you'll have to add more of these         ] 
             prime9 = currentnumber       #                                       [to increase the range. remember to change]        
         currentnumber = currentnumber + 1#                                       [ALL the relevant variables on each one.  ]
+    else:
+        print("1 isn't prime for some reason")
+        currentnumber = currentnumber + 1
