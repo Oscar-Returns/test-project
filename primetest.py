b34d4d9a34256130d00c847b3998cf03c3ada2ad
Oscar-Returns <70572014+Oscar-Returns@users.noreply.github.com>
2020-09-08 19:01:46 +0100

    Add files via upload
    works!
    not fully optimized but pretty good.

--- primetest.py
+++ primetest.py
@@ -1,32 +1,32 @@
 currentnumber = 1
-currentprime = 0
-prime = [1,1,1,1,1,1,1,1,1,1,1]
-ticklimit = 0
+prime = []
+ticklimit = -1
 check = 0
-check2 = 0
-while currentnumber < 100:
+check2 = 1
+while currentnumber < 1000:
+    check = 0
     tick = 0
-    while tick <= ticklimit and check == 0:
+    while tick <= ticklimit != -1 and check == 0:
+        check2 = 0
         if currentnumber % prime[tick] == 0 and prime[tick] != 1 and currentnumber != 1:
-            currentnumber = currentnumber + 1
-            check = 0
-            check2 = 2
-        elif tick < ticklimit and check2 == 2:
+            check = 2
+        elif tick < ticklimit:
             tick = tick + 1
+            check = 0
         else:
             check = 1
             tick = 0
 
-    if currentnumber != 1 and check == 1:
+    if currentnumber != 1 and check2 == 1 or check == 1:
         print(currentnumber)
         print("is prime")
         check = 0
-        prime[currentprime] = currentnumber
+        prime.append(currentnumber)
         currentnumber = currentnumber + 1
-        currentprime = currentprime + 1
         ticklimit = ticklimit + 1
-        print(prime)
-    else:
-        print("1 yo")
+    elif currentnumber == 1:
+        print("1 isn't prime for some reason")
         check = 0
+        currentnumber = currentnumber + 1
+    else:
         currentnumber = currentnumber + 1
